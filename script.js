88d5406c7e4ef70260a0f56d4ef1bfdd53817891
ongacarol <onga.carol@gmail.com>
2022-09-08 11:39:17 -0300

    site animado

--- script.js
+++ script.js
@@ -57,3 +57,60 @@ function initAccordion() {
     }
 }
 initAccordion();
+
+//vamos fazer um scroll suave para os links internos, pq qdo clica ele vai mto de repente e parece que mudou de página
+
+function initScrollSuave() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href'); //vai dar #contato, #faq etc, como se fosse uma id
+        const section = document.querySelector(href); //faz o contato entre link e seção
+
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+
+        //forma alternativa
+        /*const topo = section.offsetTop;
+        window.scrollTo({
+            top: topo,
+            behavior: 'smooth',
+        });*/
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection);
+    });
+}
+initScrollSuave();
+
+//quando descer a pag com scroll, fazer uma pequena animação para as sections mexerem
+
+function initAnimacaoScroll() {
+    const sections = document.querySelectorAll('.js-scroll');
+    //verificar se sections existe. Se existir, faz toda a função de animação, se não, não faz sentido que ocorra
+    if(sections.length) {
+        const windowMetade = window.innerHeight * 0.6; //para que a animação se ajuste ao tamanho da tela, está considerando 60% do tamanho da tela
+
+        function animaScroll() {
+        //primeiro, precisa saber qual a distância cada elemento está do topo. Sempre que fala "cada", usa forEach
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - windowMetade) < 0;
+            //temos que verificar se o sectionTop está < 0.
+                if (isSectionVisible) 
+                    section.classList.add('ativo');
+                else
+                    section.classList.remove('ativo'); //para não ficar animando toda hora
+            });
+        }
+
+    animaScroll(); //ativa uma vez pelo menos
+    window.addEventListener('scroll', animaScroll);
+    }
+}
+
+initAnimacaoScroll();
